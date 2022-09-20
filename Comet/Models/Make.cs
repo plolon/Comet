cee7cf4dfb4f78b5519b22668ddab6f29e99f50c
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-09-20 21:23:56 +0200

    Created models

---
+++ Comet/Models/Make.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Comet.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; } = new Collection<Model>();
+    }
+}
--- Comet/Models/Model.cs
+++ Comet/Models/Model.cs
@@ -2,6 +2,9 @@ namespace Comet.Models
 {
     public class Model
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
     }
 }
