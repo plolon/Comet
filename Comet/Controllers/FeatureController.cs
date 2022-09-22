1cad08f4ada323153f1527f8997bc0293fa7641f
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-09-22 17:15:26 +0200

    Created Feature Api Controller

---
+++ Comet/Controllers/FeatureController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Comet.DTOs;
+using Comet.Persistence.IRepositories;
+using Comet.Persistence.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Comet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureController : ControllerBase
+    {
+        private readonly IFeatureRepository featureRepository;
+        private readonly IMapper mapper;
+
+        public FeatureController(IFeatureRepository featureRepository, IMapper mapper)
+        {
+            this.featureRepository = featureRepository;
+            this.mapper = mapper;
+        }
+        // GET: api/<FeatureController>
+        [HttpGet]
+        public async Task<IEnumerable<FeatureDto>> Get()
+        {
+            var features = await featureRepository.GetAll();
+            return mapper.Map<IEnumerable<FeatureDto>>(features);
+        }
+
+        // GET api/<FeatureController>/5
+        [HttpGet("{id}")]
+        public async Task<FeatureDto> Get(int id)
+        {
+            var feature = await featureRepository.Get(id);
+            return mapper.Map<FeatureDto>(feature);
+        }
+
+        // POST api/<FeatureController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<FeatureController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<FeatureController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Comet/DTOs/FeatureDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Comet.DTOs
+{
+    public class FeatureDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
